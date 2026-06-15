38a505d8feb158478b60718a95f96692a1852824
Nguyen Hai Nam <hainamnguyen0104@gmail.com>
2026-06-15 09:24:27 +0700

    fix lại

--- xtts_api_server/tts_funcs.py
+++ xtts_api_server/tts_funcs.py
@@ -635,6 +635,7 @@ class TTSWrapper:
 
         wav = torch.cat(wavs, dim=1) if wavs else torch.zeros((1, 1), dtype=torch.float32)
         
+
         # --- POST-PROCESSING: EQ (Equalizer) ---
         import torchaudio.functional as F
         # 1. Cắt Bass MẠNH hơn
@@ -648,12 +649,10 @@ class TTSWrapper:
         # 5. Treble
         wav = F.treble_biquad(wav, sample_rate=24000, gain=2.0, central_freq=5000.0, Q=0.707)
         
-        # # --- RESAMPLE TO 44100Hz AND SAVE AS MP3 128kbps ---
-        # import torchaudio.transforms as T
-        # resampler = T.Resample(orig_freq=24000, new_freq=44100)
-        # wav_44k = resampler(wav)
-        
-        # torchaudio.save(output_file, wav_44k, 44100, format="mp3", compression=128)
+        # 6. HIỆU CHỈNH ÂM LƯỢNG TỔNG THỂ (GLOBAL GAIN)
+        wav = F.gain(wav, gain_db=1.1)
+
+        torchaudio.save(output_file, wav, 24000)
 
         generate_end_time = time.time()  # Record the time to generate TTS
         generate_elapsed_time = generate_end_time - generate_start_time
@@ -716,8 +715,7 @@ class TTSWrapper:
                 # Only a filename was provided; prepend with output folder.
                 output_file = os.path.join(self.output_folder, file_name_or_path)
 
-            if output_file.endswith('.wav'):
-                output_file = output_file[:-4] + '.mp3'
+
 
             # Check if 'text' is a valid path to a '.txt' file.
             if os.path.isfile(text) and text.lower().endswith('.txt'):
